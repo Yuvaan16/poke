3dec7a4977f89243617fe6914d871234f1607485
Yuvaan Mutreja <67225353+Yuvaan16@users.noreply.github.com>
2022-08-19 22:07:38 +0530

    a

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
