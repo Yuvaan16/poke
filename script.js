e986bd7642ad02af09582cd4e6db385084617a12
Yuvaan Mutreja <67225353+Yuvaan16@users.noreply.github.com>
2022-08-20 15:03:56 +0530

    commit idk

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const menu = document.querySelector('#menu');
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+// $(function(){
+//   $("#nav-placeholder").load("nav.html");
+// });
+
+
+function myFunction() {
+    var x = document.getElementById("click2");
+    if (x.className === "row center") {
+      void(0);
+    } else {
+      x.className = "row center";
+    }
+
+    }
+    if (x.className === "row center hidden") {
+      x.className = "row center";     
+    } else {
+      x.className = "row center hidden";
+    }
